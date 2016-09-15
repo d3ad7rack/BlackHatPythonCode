9187b2aa3e643d844e76627acf85dc1c4288452e
d3ad7rack <william.o.frye@gmail.com>
2016-09-15 12:27:30 -0400

    added a simple tcp server and netcat-like alternative, pycat

--- simple-tcp-client.py
+++ simple-tcp-client.py
@@ -2,28 +2,20 @@ import socket
 import sys
 
 
-def main():
+# Print usage if no arguments are given
+if len(sys.argv) < 3:
+    print "Usage: python simple-client.py <IP/Domain> <TCP Port>"
+else:
     # Takes arguments from the CLI to find out what server you want to interact with and what port
     target_host = sys.argv[1]
     target_port = int(sys.argv[2])
-
     # Create a socket object
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # Connect the client
     client.connect((target_host,target_port))
-
     # Send some data
     client.send("GET / HTTP/1.1\r\nHost: " + target_host + "\r\n\r\n")
-
     # Receive some data
     response = client.recv(4096)
-
     # Print out response from server
     print response
-
-# Print usage if no arguments are given
-if len(sys.argv) < 2:
-    print "Usage: python simple-client.py <IP/Domain> <TCP Port>"
-else:
-    main()
---
+++ tcp-server.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+import sys
+
+
+if len(sys.argv) < 3:
+    print "Usage: python tcp-server.py <IP to bind to> <Port to bind to>"
+else:
+    bind_ip = sys.argv[1]
+    bind_port = int(sys.argv[2])
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((bind_ip,bind_port))
+    server.listen(5)
+    print "[*] Listening on %s:%d" % (bind_ip,bind_port)
+
+# This is the client-handling thread
+def handle_client(client_socket):
+    # Print out what the client sends
+    request = client_socket.recv(4096)
+    print "[*] Received: %s" % request
+    # Send back a packet
+    client_socket.send("ACK!!\r\n")
+    client_socket.close()
+
+while True:
+    client,addr = server.accept()
+    print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
+    # Spin up client thread to handle incoming data
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
